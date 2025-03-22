d97903d21313e94971d280a8d27ba5774ea9e449
Davs07 <botsymeisterspieler@gmail.com>
2025-03-22 17:37:57 -0500

    build: update project configuration and dependencies
    - Add next.config.js and update next.config.ts with new options
    - Modify tsconfig.json to include tailwind.config.ts and adjust paths
    - Update package.json with new dependencies, devDependencies, and scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // output: 'export',
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: { unoptimized: true },
+  };
+  
+  module.exports = nextConfig;
+  
